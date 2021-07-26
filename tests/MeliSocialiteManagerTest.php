2da26e6e76692133ff89892cc1b2d9b578bfe338
Angel Martinez <angel.martz24@gmail.com>
2021-07-25 21:41:50 -0500

    Added PHPUnit Tests

---
+++ tests/MeliSocialiteManagerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Laravel\Socialite\Tests;
+
+use Laravel\Socialite\Contracts\Factory;
+use Laravel\Socialite\SocialiteServiceProvider;
+use Altivium\MeliSocialite\MeliProvider;
+use Laravel\Socialite\Two\GithubProvider;
+use Orchestra\Testbench\TestCase;
+
+class MeliSocialiteManagerTest extends TestCase
+{
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('services.meli', [
+            'client_id' => 'meli-client-id',
+            'client_secret' => 'meli-client-secret',
+            'redirect' => 'http://your-callback-url',
+        ]);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [SocialiteServiceProvider::class];
+    }
+
+    public function test_it_can_instantiate_the_meli_driver()
+    {
+        $factory = $this->app->make(Factory::class);
+
+        $provider = $factory->driver('meli');
+
+        $this->assertInstanceOf(MeliProvider::class, $provider);
+    }
+}
